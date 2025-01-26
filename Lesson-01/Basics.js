a71ae36c215ac1da5e2a9aafeccf6f01f91ce21a
Prajwal S <wizq@Prajwals-MacBook-Air.local>
2025-01-26 10:28:27 +0530

    Added Lesson 01

---
+++ Lesson-01/Basics.js
@@ -0,0 +1,49 @@
+// Here's The Basics Commands Of Javascript Which Is Used In Web Browser..
+// This Commands Can Be Ran In Web Browswer Console...
+// To Open A Web Browswer Console 
+    // In Windows You Can Right Click And Press Inspect Tools And Go To Console.. 
+        // Or Press CLRT + SHIFT + I
+
+    // In Mac You Can Right Click And Press Inspect Tools And Go To Console.
+        // Or Press Command + Option + I
+        
+        
+alert('Hello World!'); // So Basically This "alert" Feature Sends A Popup On Web Saying Hello World
+
+// You Can Change The Text In Between To Anything You Want To Display
+alert('Good Moring!') // So This Changes The Popup Message "Hello World!" Message To.. "Good Moring!"
+
+
+// Here's Some Basic Maths You Can Do In Console 
+2 + 2 // When You Type This You Will Get The Answer As 4
+// 4 
+10 - 7 
+// 3 
+ 
+
+// More Things Javascript Can Do...
+
+document.body.innerHtml = 'Hello World'; // Javascript Is Case Sensitive
+// What This Code Does. It Removes Everything On The Web Page Your On..
+ 
+
+// Syntax's
+
+// alert'Hello') 
+    // If You Run This Line Of Code You Will Get A Syntax Error Becaue. ( Is Missing
+
+    
+
+// Basics Of Javascript
+
+    // - Giving Instructions To Computer (Code)
+    // - The Computer Follows Our Instructions (Running The Code)
+
+
+// What We Learnt.. 
+
+    // 1. Javascript Is Giving Instructions To A Computer
+    // 2. Wrote Javascript Code
+    // 3. Run Our Code In Console.
+    // 4. Leant About Syntax
+    
---
+++ Lesson-01/Questions-And-Answers.js
@@ -0,0 +1,41 @@
+// Note: Do This Questions On Your Broswer Console 
+// Note2: If Your Copy Pasting, You May Get Waring Of Saying Pasting Is Disabled Please Type "allow pasting" In Console And Press Enter
+
+// 1a. Use "alert(...);" To Display "Good Evening!" In Popup 
+alert('Good Evening!');
+
+// 1b. Display Your Name In Popup
+alert('Wizq');
+
+// 1c. Use Math Caculate 10 + 5 In The Console
+// 10 + 5
+15
+
+// 1d. Caculate 20 - 5 In Cosnole
+// 20 - 5
+15
+
+// 1e. Caculate 2 + 2 - 5
+// 2 + 2 - 5 
+-1
+
+// 1f. Use "document.body.innerHTML = ...;" To Display "Good Moring" On The Webpage
+document.body.innerHTMLL = "Good Moring!";
+
+// 1g. Display Your Name On Web Page
+doucument.body.innerHTMLL = "Wizq";
+
+
+
+// Challenge Exerises
+
+// 1h. You Order A T-Shirt For $10, Socks For $8, And Dinner Plates For $20 Use Javascript To Caculate The Total Cost Of Your Order
+// 10 + 8 + 20
+38
+
+// 1I. Your Bank Account Hass $100, You Spend $20 On Lunch, $50 On Dinner, And Earn $200 From Your Job Calculate How Much Money You Have
+// (100 - 20 - 50 ) + 200
+330
+
+// 1j. Use "document.body.innerHTML = ...;" To Make The Webpage Blank
+document.body.innerHTML = ""; // This Makes The Webpage Blank
