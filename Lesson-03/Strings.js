ca7f09540d6d01120e8811693000bc2919155847
Prajwal <prajwal2079@gmail.com>
2025-03-20 19:38:28 +0530

    Added New Lesson

---
+++ Lesson-03/Strings.js
@@ -0,0 +1,56 @@
+// Basics Of Strings In Javascript
+// A String = Text
+
+'Hello';
+// This Is A String Which Represet Hello
+
+// If You Remeber In First Lesson We Used String Inside "alert"
+alert('Hello');
+// The Word Hello Is String
+
+
+// Syntax Rules For Strings
+// To Create A String We Write Any Word Example Hello We Suround Them With Single Quotes Like
+'Hello';
+
+// So If We Type
+'Some' + 'text';
+// The Output is 'Sometext'
+// When We Add 2 Strings It Combine Together To A Bigger Strings
+
+// We Can Add More Than 2 Strings Example
+'Some' + 'more' + 'text';
+// The Output is 'Somemoretext'
+// This Is Called "Concatenation" When We Combine Strings 
+
+
+// Types Of Strings
+// We Can Check What Type Of Value It Is.. By Using
+typeof '';
+
+//Examples
+    typeof 2;
+    // Output = 'number'
+
+    typeof 'Hello'
+    // Output = 'string'
+
+
+// What If We Add String + Number? 
+// Example
+    'Hello' + 2
+    // Output 'Hello2'
+    
+// So When We Add A String And Number Javascript Automatically Converts It To A String 
+
+// 3 Ways To Create A String 
+'Hello';
+"Hello";
+`Hello`;
+
+// The Backtick ` Is Called Template Literal
+// It Allows Us To Write A String In Multiple Lines
+
+
+
+// Continue With Questions-And-Answers.js   
